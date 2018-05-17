0bf568281eb9770127a96bc9d00a753288bdcf43
Neil Tan <neil.c.tan@gmail.com>
2018-05-18 01:09:24 +0800

    working

--- main.cpp
+++ main.cpp
@@ -8,9 +8,7 @@
 #include "MMA7660FC.h"
 #include "SensorQueue.hpp"
 
-#define ADDR_MMA7660 0x98                   // I2C SLAVE ADDR MMA7660FC
-MMA7660FC Acc(I2C_SDA, I2C_SCL, ADDR_MMA7660);      //sda, scl, Addr
-float G_VALUE[64] = { 0, 0.047, 0.094, 0.141, 0.188, 0.234, 0.281, 0.328,
+static const float G_VALUE[64] = { 0, 0.047, 0.094, 0.141, 0.188, 0.234, 0.281, 0.328,
                       0.375, 0.422, 0.469, 0.516, 0.563, 0.609, 0.656, 0.703,
                       0.750, 0.797, 0.844, 0.891, 0.938, 0.984, 1.031, 1.078,
                       1.125, 1.172, 1.219, 1.266, 1.313, 1.359, 1.406, 1.453,
@@ -19,6 +17,8 @@ float G_VALUE[64] = { 0, 0.047, 0.094, 0.141, 0.188, 0.234, 0.281, 0.328,
                       -0.844, -0.797, -0.750, -0.703, -0.656, -0.609, -0.563,
                       -0.516, -0.469, -0.422, -0.375, -0.328, -0.281, -0.234,
                       -0.188, -0.141, -0.094, -0.047};
+
+MMA7660FC Acc(I2C_SDA, I2C_SCL, ADDR_MMA7660);      //sda, scl, Addr
 
 Serial pc(USBTX, USBRX, 115200);
 // F413ZH_SD_BlockDevice bd;
@@ -54,17 +54,17 @@ int main() {
   while(1){
     float x=0, y=0, z=0;
     
-    Acc.read_Tilt(&x, &y, &z);                                  // Read the acceleration                    
-    pc.printf("Tilt x: %2.2f degree \n", x);                    // Print the tilt orientation of the X axis
-    pc.printf("Tilt y: %2.2f degree \n", y);                    // Print the tilt orientation of the Y axis
-    pc.printf("Tilt z: %2.2f degree \n", z);                    // Print the tilt orientation of the Z axis
+    // Acc.read_Tilt(&x, &y, &z);                                  // Read the acceleration                    
+    // pc.printf("Tilt x: %2.2f degree \n", x);                    // Print the tilt orientation of the X axis
+    // pc.printf("Tilt y: %2.2f degree \n", y);                    // Print the tilt orientation of the Y axis
+    // pc.printf("Tilt z: %2.2f degree \n", z);                    // Print the tilt orientation of the Z axis
  
-    wait_ms(100);
+    // wait_ms(100);
  
     pc.printf("x: %1.3f g \n", G_VALUE[Acc.read_x()]);          // Print the X axis acceleration
     pc.printf("y: %1.3f g \n", G_VALUE[Acc.read_y()]);          // Print the Y axis acceleration
     pc.printf("z: %1.3f g \n", G_VALUE[Acc.read_z()]);          // Print the Z axis acceleration
     pc.printf("\n");
-    wait(1);
+    wait(0.5);
   }
 }
